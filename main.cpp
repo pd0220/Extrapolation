686c98dd36060626154738931f0901fca671c293
pd0220 <david.pesznyak@gmail.com>
2020-11-16 10:40:04 +0100

    initialise repository

---
+++ main.cpp
@@ -0,0 +1,15 @@
+// extrapolation to real chemical potentials for lattice QCD simulations
+// using previously determined sector coefficients
+
+// used headers and libraries
+#include <iostream>
+#include <fstream>
+#include <iostream>
+
+// main function
+// argv[1] --> file name to use
+int main(int argc, char **argv)
+{
+    // reading argument list
+    std::string fileName = argv[1];
+}
