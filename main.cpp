9c2e355d66f82b9cdf23dd13ea7a464f0a56140a
pd0220 <david.pesznyak@gmail.com>
2020-11-18 17:35:16 +0100

    parsing, structure and spelling update

--- main.cpp
+++ main.cpp
@@ -14,10 +14,10 @@
 
 // constants
 int const maxIteration = 10000;
-int const muBNum = 75;
+int const muBNum = 200;
 double const eps = 1e-10;
-double const muBMin = 0.;
-double const muBMax = 1.75;
+double const muBMin = 0.01;
+double const muBMax = 2.00;
 
 // ---------------------------------------------------------------------------------------
 
@@ -167,10 +167,10 @@ int main(int argc, char **argv)
     // sector coefficients at fixed temperature
     std::string const fileName = argv[1];
     // where to cut sector (B = 2 or 3)
-    int const SecturCut = std::atoi(argv[2]);
-    if (SecturCut != 2 && SecturCut != 3)
+    int const SectorCut = std::atoi(argv[2]);
+    if (SectorCut != 2 && SectorCut != 3)
     {
-        std::cout << "ERROR\nSector cut parameter is not appropriate: instead of " << SecturCut << " it should be 2 or 3" << std::endl;
+        std::cout << "ERROR\nSector cut parameter is not appropriate: instead of " << SectorCut << " it should be 2 or 3" << std::endl;
         std::exit(-2);
     }
 
@@ -186,7 +186,7 @@ int main(int argc, char **argv)
 
     // used sectors
     std::vector<std::pair<int, int>> SectorNumbers;
-    if (SecturCut == 2)
+    if (SectorCut == 2)
         SectorNumbers = {{1, 0}, {0, 1}, {1, -1}, {1, 1}, {1, 2}, {1, 3}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {0, 2}, {0, 3}};
     else
         SectorNumbers = {{1, 0}, {0, 1}, {1, -1}, {1, 1}, {1, 2}, {1, 3}, {2, 0}, {2, 1}, {2, 2}, {2, 3}, {0, 2}, {0, 3}, {3, 0}, {3, 1}, {3, 2}, {3, 3}};
@@ -199,7 +199,7 @@ int main(int argc, char **argv)
         {
             // Newton iteration to find roots
             // 0th step
-            double guess = 0.5;
+            double guess = 1.;
             // 1st step
             double res = guess - ZFuncReal(muB(iMuB), guess, 0, 1, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(iMuB), guess, 0, 2, SectorNumbers, PMatrix.col(iSample));
             // start iteration
@@ -230,12 +230,16 @@ int main(int argc, char **argv)
         // errors from jackknife samples
         for (int iSample = 0; iSample < PMatrix.cols() - 1; iSample++)
         {
-            Z12Err(i, iSample) = ZFuncReal(muB(i), muS(i, iSample), 1, 0, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(i), muS(i, iSample), 2, 0, SectorNumbers, PMatrix.col(iSample));
-            Z31Err(i, iSample) = ZFuncReal(muB(i), muS(i, iSample), 3, 0, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(i), muS(i, iSample), 1, 0, SectorNumbers, PMatrix.col(iSample));
-            Z42Err(i, iSample) = ZFuncReal(muB(i), muS(i, iSample), 4, 0, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(i), muS(i, iSample), 2, 0, SectorNumbers, PMatrix.col(iSample));
+            Z12Err(i, iSample) = ZFuncReal(muB(i), muS(i, iSample + 1), 1, 0, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(i), muS(i, iSample + 1), 2, 0, SectorNumbers, PMatrix.col(iSample));
+            Z31Err(i, iSample) = ZFuncReal(muB(i), muS(i, iSample + 1), 3, 0, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(i), muS(i, iSample + 1), 1, 0, SectorNumbers, PMatrix.col(iSample));
+            Z42Err(i, iSample) = ZFuncReal(muB(i), muS(i, iSample + 1), 4, 0, SectorNumbers, PMatrix.col(iSample)) / ZFuncReal(muB(i), muS(i, iSample + 1), 2, 0, SectorNumbers, PMatrix.col(iSample));
         }
 
         // write to screen
-        std::cout << muB(i) << " " <<  Z12(i) << " " << std::sqrt(JCKVariance(Z12Err.row(i))) << " " << Z31(i) << " " << std::sqrt(JCKVariance(Z31Err.row(i))) << " " << Z42(i) << " " << std::sqrt(JCKVariance(Z42Err.row(i))) << std::endl;
+        std::cout << muB(i) << " "
+                  << muS.col(0)(i) << " " << std::sqrt(JCKVariance(muS.row(i).segment(1, PMatrix.cols() - 1))) << " "
+                  << Z12(i) << " " << std::sqrt(JCKVariance(Z12Err.row(i))) << " "
+                  << Z31(i) << " " << std::sqrt(JCKVariance(Z31Err.row(i))) << " "
+                  << Z42(i) << " " << std::sqrt(JCKVariance(Z42Err.row(i))) << std::endl;
     }
 }
